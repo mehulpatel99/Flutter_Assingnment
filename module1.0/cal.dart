e10599b06cbfbd5fb43d701cb31de60ff31d90a7
mehulpatel99 <138190866+mehulpatel99@users.noreply.github.com>
2023-08-14 18:11:09 +0530

    Add files via upload

---
+++ module1.0/cal.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  print("enter your 1 number: ");
+  int? num1 = int.parse(stdin.readLineSync()!);
+  print("enter your 2 number: ");
+  int? num2 = int.parse(stdin.readLineSync()!);
+
+  int num3 = num1 + num2;
+  print("your add is = $num3");
+
+  int num4 = num1 - num2;
+  print("your sub is = $num4");
+
+  int num5 = num1 * num2;
+  print("your multiply is = $num5");
+
+  double num6 = num1 / num2;
+  print("your divide is = $num6");
+}
